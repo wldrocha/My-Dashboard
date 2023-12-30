dc88929d0cfc213900be997f7e3b9c3b4098fc56
Wladimir Rocha <wld.rocha@gmail.com>
2023-12-30 18:53:11 -0500

    [REF]part on components

---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,12 @@
+// import { redirect } from 'next/navigation'
+
+
+export default function CounterPage() {
+//   redirect('/dashboard/counter')
+
+  return (
+    <div>
+      <h1>Counter Page</h1>
+    </div>
+  )
+}
