f879cc69630521676510d5c336601bc8b84705ee
Wladimir Rocha <wld.rocha@gmail.com>
2023-12-30 20:28:09 -0500

    [ADD] sidebar menu items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import Image from 'next/image'
+import { redirect } from 'next/navigation'
 
 export default function Home() {
-  return (
-    <>
-      <h1>Hola mundo</h1>
-    </>
-  )
+  redirect('/dashboard/main')
 }
