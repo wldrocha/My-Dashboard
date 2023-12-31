fb53f61050bc2a8418f08cc6dbbf2039308eaf64
Wladimir Rocha <wld.rocha@gmail.com>
2023-12-31 15:01:49 -0500

    [ADD] particular not found on pokemons

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,8 +5,8 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
     <>
       <div className='bg-slate-100 overflow-y-scroll w-screen h-screen antialiased text-slate-300 selection:bg-blue-600 selection:text-white'>
         <div className='flex'>
-         <Sidebar />
-          <div className='p-2 w-full text-slate-900'>{children}</div>
+          <Sidebar />
+          <div className='w-full text-slate-900'>{children}</div>
         </div>
       </div>
     </>
